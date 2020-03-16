bb2f990a69dbca550a080528acbd9bf9d84d4ae2
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-03-17 00:54:27 +0100

    pass import string to resolver

--- src/transformers/default-to-named-import.ts
+++ src/transformers/default-to-named-import.ts
@@ -1,4 +1,6 @@
-import { API, FileInfo, Identifier, Options } from 'jscodeshift/src/core';
+import * as K from 'ast-types/gen/kinds';
+import * as N from 'ast-types/gen/nodes';
+import { API, FileInfo, Identifier, ImportDeclaration, Options, StringLiteral } from 'jscodeshift/src/core';
 import { defaultExportNameResolver, ExportNameResolver } from './default-to-named';
 import { DoNotTransform } from './jscodeshift-constants';
 
@@ -11,15 +13,36 @@ export const transform = (file: FileInfo, api: API, _options: Options, exportNam
   }
   const j = api.jscodeshift;
   const root = j(file.source);
-  root.find(j.ImportDefaultSpecifier).forEach((defaultImport) => {
-    let exportName = exportNameResolver({}, {});
-    let localNameIdentifier: Identifier | null = null;
-    let localName = defaultImport.value.local?.name;
-    if ( localName !== undefined && localName !== exportName) {
-      localNameIdentifier = j.identifier(localName);
-    }
-    defaultImport.replace(j.importSpecifier(j.identifier(exportName), localNameIdentifier));
+  root.find(j.ImportDeclaration).forEach(importDeclaration => {
+    j(importDeclaration).find(j.ImportDefaultSpecifier).forEach((defaultImport) => {
+      const importString = extractImportString(importDeclaration.node);
+      let exportName = exportNameResolver({ path: file.path, importString });
+      let localNameIdentifier: Identifier | null = null;
+      let localName = defaultImport.value.local?.name;
+      if ( localName !== undefined && localName !== exportName) {
+        localNameIdentifier = j.identifier(localName);
+      }
+      defaultImport.replace(j.importSpecifier(j.identifier(exportName), localNameIdentifier));
+    })
   });
   return root.toSource({ quote: 'single' });
 };
 
+const extractImportString = (importDeclaration: ImportDeclaration): string => {
+  let source = importDeclaration.source;
+  if (isStringLiteral(source)) {
+    return source.value;
+  }
+  if(isLiteral(source) && typeof source.value === 'string'){
+    return source.value;
+  }
+  throw new Error(`Unable to extract import string from import declaration of source type ${source.type}`)
+};
+
+const isStringLiteral = (toCheck: K.LiteralKind): toCheck is K.StringLiteralKind => {
+  return toCheck.type === 'StringLiteral';
+};
+
+const isLiteral = (toCheck: K.LiteralKind): toCheck is N.Literal => {
+  return toCheck.type === 'Literal';
+};
--- src/transformers/default-to-named.ts
+++ src/transformers/default-to-named.ts
@@ -3,13 +3,12 @@ import { ASTPath, ExportDefaultDeclaration } from 'jscodeshift/src/core';
 import { basename, extname } from 'path';
 
 export type ExportName = string;
-export type Importer = {};
-export type Exporter = {};
+export type Importer = { path: string, importString: string };
 
 /*
  if resolving imports becomes tedious have a look at https://github.com/dividab/tsconfig-paths
  */
-export type ExportNameResolver = (importer: Importer, exporter: Exporter) => ExportName
+export type ExportNameResolver = (importer: Importer) => ExportName
 
 export const defaultExportNameResolver: ExportNameResolver = (): ExportName => {
   return 'StandInExportName';
