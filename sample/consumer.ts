11b888691a3d28ec7b6e04ae8955f5a7e56655c5
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-05-16 21:48:23 +0200

    add another example encountered in the wild

--- sample/consumer.ts
+++ sample/consumer.ts
@@ -3,9 +3,11 @@ import theOne from './default-export-string-literal';
 import fruit from './default-export-object-literal';
 import sneaky from './default-export-using-as-rename';
 import type JustAClass from './default-export-class'
+import someFunction from './default-export-function';
 
 const localVariable: JustAClass | undefined = undefined;
 console.log(theOne);
 console.log(fruit);
 console.log(sneaky);
 console.log(localVariable);
+console.log(someFunction())
---
+++ sample/default-export-function.ts
@@ -0,0 +1,5 @@
+export const someFunction = () => {
+  return 'wohooo';
+}
+
+export default someFunction;
