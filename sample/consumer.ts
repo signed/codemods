11944ac3bdf03f913e84d1f13d6e5bca2c32a45b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-05-19 01:29:36 +0200

    real code is so strange

--- sample/consumer.ts
+++ sample/consumer.ts
@@ -3,9 +3,11 @@ import theOne from './default-export-string-literal';
 import fruit from './default-export-object-literal';
 import type JustAClass from './default-export-class'
 import someFunction from './default-export-function';
+import { default as anotherName } from './default-export-function';
 
 const localVariable: JustAClass | undefined = undefined;
 console.log(theOne);
 console.log(fruit);
 console.log(localVariable);
-console.log(someFunction())
+console.log(someFunction());
+console.log(anotherName());
